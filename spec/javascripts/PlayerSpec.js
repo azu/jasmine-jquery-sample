78225c8c7467ea648233de68feac9cf0f6486dc3
azu <azuciao@gmail.com>
2011-04-12 00:43:51 +0900

    loadは同期的に行われているので修正

--- spec/javascripts/PlayerSpec.js
+++ spec/javascripts/PlayerSpec.js
@@ -1,8 +1,9 @@
 $(function() {
     jasmine.getFixtures().fixturesPath = '/';// 読み込みパスはroot
-    jasmine.getFixtures().load('index.html');// 対象ページ
     describe("index.html", function() {
-        console.log(document.readyState)
+        console.log(document.readyState);
+        // ロードは同期的に行われている
+        jasmine.getFixtures().load('index.html');// 対象ページ
         it("HTMLがロードできている", function() {
             console.log($('#main').text())
         })
