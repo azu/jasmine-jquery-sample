66ebfe54052133e091473f116f4848fc0cfc581e
azu <azuciao@gmail.com>
2011-04-12 00:54:47 +0900

    テストをまともに

--- spec/javascripts/PlayerSpec.js
+++ spec/javascripts/PlayerSpec.js
@@ -2,10 +2,10 @@ $(function() {
     jasmine.getFixtures().fixturesPath = '/';// 読み込みパスはroot
     describe("index.html", function() {
         console.log(document.readyState);
-        // ロードは同期的に行われている
-        jasmine.getFixtures().load('index.html');// 対象ページ
         it("HTMLがロードできている", function() {
-            console.log($('#main').text())
+            // ロードは同期的に行われている
+            jasmine.getFixtures().load('index.html');// 対象ページ
+            expect($('#main')).toHaveText("文章")
         })
     })
 })
